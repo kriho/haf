5378ff425faaf863c579bb6969204bdc3254a0ce
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-03-01 09:19:36 +0100

    added model

---
+++ Observable/Model.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Composition;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HAF {
+  public class Model : ObservableObject {
+  }
+}
