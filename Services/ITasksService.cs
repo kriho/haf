35811491d971bb523085b07482fe41bfe356829d
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-03-02 16:37:16 +0100

    added tasks service

---
+++ Services/ITasksService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace HAF {
+  public interface ITasksService: IService {
+    ObservableTaskPool this[string name] { get; }
+    ObservableCollection<ObservableTaskPool> TaskPools { get; set; }
+    void AddTaskPool(string name, bool allowParallelExecution);
+  }
+}
---
+++ Services/TasksService.cs
@@ -0,0 +1,38 @@
+﻿using HAF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.Composition;
+using System.Deployment.Application;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Xml;
+
+namespace HAF {
+
+  [Export(typeof(ITasksService)), PartCreationPolicy(CreationPolicy.Shared)]
+  public class TasksService: Service, ITasksService {
+
+    public ObservableCollection<ObservableTaskPool> TaskPools { get; set; } = new ObservableCollection<ObservableTaskPool>();
+
+    public ObservableTaskPool this[string name] {
+      get { return this.TaskPools.FirstOrDefault(t => t.Name == name); }
+    }
+
+    protected override void Initialize() {
+    }
+
+    public void AddTaskPool(string name, bool allowParallelExecution) {
+      if (this.TaskPools.Any(t => t.Name == name)) {
+        throw new Exception($"the task domain {name} already exists");
+      }
+      this.TaskPools.Add(new ObservableTaskPool() {
+        Name = name,
+        AllowParallelExecution = allowParallelExecution,
+      });
+    }
+  }
+}
