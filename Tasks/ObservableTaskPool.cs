c00235ff42e7c2fb9685c64bd9d6d8762b5cbe29
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-03-02 16:36:53 +0100

    changed control scheme of task pools

--- Tasks/ObservableTaskPool.cs
+++ Tasks/ObservableTaskPool.cs
@@ -1,37 +1,44 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace HAF {
-  public class ObservableTaskPool {
-    private static ObservableTaskPool defaultInstance = null;
-    public static ObservableTaskPool Default {
-      get {
-        // initialize when needed
-        if (defaultInstance == null) {
-          defaultInstance = new ObservableTaskPool();
-        }
-        return defaultInstance;
+  public class ObservableTaskPool: LinkedObject {
+    public string Name { get; set; }
+
+    public bool AllowParallelExecution { get; set; }
+
+    public LinkedState CanScheduleTasks { get; private set; } = new LinkedState();
+
+    public ObservableCollection<ObservableTask> Tasks { get; private set; } = new ObservableCollection<ObservableTask>();
+
+    protected override void Initialize() {
+      this.Link = this.Name;
+    }
+
+    public void ScheduleTask(ObservableTask task) {
+      if(this.Tasks.Count > 0 && !this.AllowParallelExecution) {
+        throw new Exception("task group does not allow parallel execution");
+      }
+      task.AssignPool(this);
+      this.Tasks.Add(task);
+      if(!this.AllowParallelExecution) {
+        this.CanScheduleTasks.Value = false;
       }
     }
 
-    public ObservableCollection<ObservableTask> Tasks { get; private set; }
-
-    public ObservableTaskPool() {
-      this.Tasks = new ObservableCollection<ObservableTask>();
-    }
-
-    internal void RegisterTask(ObservableTask task) {
-      this.Tasks.Add(task);
-    }
-
-    internal void UnregisterTask(ObservableTask task) {
-      this.Tasks.Remove(task);
+    public void UnscheduleTask(ObservableTask task) {
+      if(this.Tasks.Contains(task)) {
+        this.Tasks.Remove(task);
+        if (this.AllowParallelExecution || this.Tasks.Count == 0) {
+          this.CanScheduleTasks.Value = true;
+        }
+      }
     }
   }
 }
