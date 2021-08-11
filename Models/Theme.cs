47724807a6db5164812ac322cbfcb86b3aab5e19
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-08-11 21:27:07 +0200

    renamed colors, added brushes

--- Models/Theme.cs
+++ Models/Theme.cs
@@ -8,10 +8,13 @@ using System.Windows.Media;
 namespace HAF.Models {
   public class Theme {
     public string Name { get; set; }
+    public Color BackgroundColor {get; set;}
+    public Color TextColor {get; set;}
     public Color AccentColor { get; set; }
-    public Color MainColor {get; set;}
-    public Color BasicColor {get; set;}
+    public Color LightColor {get; set;}
     public Color StrongColor {get; set;}
-    public Color MarkerColor {get; set;}
+    public Color InfoColor {get; set;}
+    public Color WarningColor {get; set;}
+    public Color ErrorColor {get; set;}
   }
 }
--- Services/IThemesService.cs
+++ Services/IThemesService.cs
@@ -4,15 +4,26 @@ using System.Windows.Media;
 
 namespace HAF {
   public interface IThemesService : IService {
-    RelayCommand<Theme> DoSetTheme { get; }
+    Color BackgroundColor { get; }
+    Brush BackgroundBrush { get; }
+    Color TextColor { get; }
+    Brush TextBrush { get; }
     Color AccentColor { get; }
+    Brush AccentBrush { get; }
+    Color LightColor { get; }
+    Brush LightBrush { get; }
+    Color StrongColor { get; }
+    Brush StrongBrush { get; }
+    Color InfoColor { get; }
+    Brush InfoBrush { get; }
+    Color WarningColor { get; }
+    Brush WarningBrush { get; }
+    Color ErrorColor { get; }
+    Brush ErrorBrush { get; }
+    RelayCommand<Theme> DoSetTheme { get; }
     Theme ActiveTheme { get; set; }
     NotifyCollection<Theme> AvailableThemes { get; }
-    Color BasicColor { get; }
     LinkedDependency MayChangeTheme { get; }
-    Color MainColor { get; }
-    Color MarkerColor { get; }
     LinkedEvent OnActiveThemeChanged { get; }
-    Color StrongColor { get; }
   }
 }
