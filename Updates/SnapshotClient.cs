8083ce5513857cddd2cfba183961e1614ff1787f
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-08-19 16:20:23 +0200

    use new observable task scheme

--- Updates/SnapshotClient.cs
+++ Updates/SnapshotClient.cs
@@ -104,11 +104,11 @@ namespace HAF.Updates {
       }
     }
 
-    public Snapshot DownloadHeadSnapshot(string applicationPath, ObservableTask task = null) {
+    public async Task<Snapshot> DownloadHeadSnapshot(string applicationPath, CancellationToken cancellationToken, ITaskProgress progress) {
       Snapshot snapshot = null;
       this.Reporter.StartSession(Messages.StartDownloadHead);
-      task.ReportIndeterminate(this.GetMessageText(Messages.StartDownloadHead));
-      using (var stream = this.FtpClient.DownloadToStream(FtpClient.CombinePath(applicationPath, Snapshot.FileName))) {
+      progress.ReportIndeterminate(this.GetMessageText(Messages.StartDownloadHead));
+      using (var stream = await this.FtpClient.DownloadToStream(FtpClient.CombinePath(applicationPath, Snapshot.FileName), cancellationToken, progress: progress)) {
         if (this.Reporter.HasErrors) {
           return null;
         }
@@ -125,12 +125,6 @@ namespace HAF.Updates {
       }
     }
 
-    public Task<Snapshot> DownloadHeadSnapshotAsync(string applicationPath, ObservableTask task = null) {
-      return Task.Run(() => {
-        return this.DownloadHeadSnapshot(applicationPath, task);
-      });
-    }
-
     private void SearchDirectories(string directory, ref List<string> subs, char seperator) {
       if (directory.Contains(seperator)) {
         var sub = directory.Substring(0, directory.LastIndexOf(seperator));
@@ -141,7 +135,7 @@ namespace HAF.Updates {
       }
     }
 
-    public void UploadSnapshot(Snapshot snapshot, DirectoryInfo directory, ObservableTask task = null) {
+    public async Task UploadSnapshot(Snapshot snapshot, DirectoryInfo directory, CancellationToken cancellationToken, ITaskProgress progress) {
       this.Reporter.StartSession(Messages.StartUploadSnapshot);
       // check snapshot
       if (snapshot == null) {
@@ -156,23 +150,23 @@ namespace HAF.Updates {
       // sort by depth
       subs = subs.OrderBy(s => s.Count(c => c == '/')).ToList();
       // task count is now known
-      task?.ReportProgress(0, snapshot.Files.Count + subs.Count, this.GetMessageText(Messages.StartUploadSnapshot));
+      progress?.ReportProgress(0, snapshot.Files.Count + subs.Count, this.GetMessageText(Messages.StartUploadSnapshot));
       // create subdirectories
       foreach (var sub in subs) {
         if (sub.Contains("/")) {
           // check if directory exists
-          var directoryExists = this.FtpClient.DirectoryExists(sub.Substring(0, sub.LastIndexOf('/')), sub.Substring(sub.LastIndexOf('/') + 1));
+          var directoryExists = await this.FtpClient.DirectoryExists(sub.Substring(0, sub.LastIndexOf('/')), sub.Substring(sub.LastIndexOf('/') + 1));
           if (!directoryExists) {
-            this.FtpClient.CreateDirectory(FtpClient.CombinePath(sub));
+            await this.FtpClient.CreateDirectory(FtpClient.CombinePath(sub));
           }
         } else {
           // check if directory exists
-          var directoryExists = this.FtpClient.DirectoryExists("", sub);
+          var directoryExists = await this.FtpClient.DirectoryExists("", sub);
           if (!directoryExists) {
-            this.FtpClient.CreateDirectory(sub);
+            await this.FtpClient.CreateDirectory(sub);
           }
         }
-        task?.ReportProgress();
+        progress?.ReportProgress();
       }
       if (this.Reporter.HasErrors) {
         return;
@@ -183,10 +177,10 @@ namespace HAF.Updates {
           if (this.Reporter.HasErrors) {
             return;
           }
-          task?.ReportProgress("Uploading " + file.RemotePath);
-          var success = this.FtpClient.UploadFileVerified(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, file.RemotePath), Path.Combine(directory.FullName, file.RemotePath), file.MD5, file.Size);
+          progress?.ReportProgress("Uploading " + file.RemotePath);
+          var success = await this.FtpClient.UploadFileVerified(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, file.RemotePath), Path.Combine(directory.FullName, file.RemotePath), file.MD5, cancellationToken, file.Size, progress);
           if (success) {
-            task?.ReportProgress();
+            progress?.ReportProgress();
             break;
           } else {
             tries++;
@@ -198,92 +192,82 @@ namespace HAF.Updates {
         }
       }
       var snapshotFileInfo = new FileInfo(Path.Combine(directory.FullName, Snapshot.FileName));
-      task?.ReportProgress("Uploading the snapshot file");
-      this.FtpClient.UploadFile(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, Snapshot.FileName), snapshotFileInfo.FullName, snapshotFileInfo.Length);
-      task?.ReportProgress();
-      task?.ReportProgress("Uploading the snapshot file as head");
-      this.FtpClient.UploadFile(FtpClient.CombinePath(snapshot.ApplicationPath, Snapshot.FileName), snapshotFileInfo.FullName, snapshotFileInfo.Length);
-      task?.ReportProgress();
+      progress?.ReportProgress("Uploading the snapshot file");
+      await this.FtpClient.UploadFile(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, Snapshot.FileName), snapshotFileInfo.FullName, cancellationToken, snapshotFileInfo.Length, progress);
+      progress?.ReportProgress();
+      progress?.ReportProgress("Uploading the snapshot file as head");
+      await this.FtpClient.UploadFile(FtpClient.CombinePath(snapshot.ApplicationPath, Snapshot.FileName), snapshotFileInfo.FullName, cancellationToken, snapshotFileInfo.Length, progress);
+      progress?.ReportProgress();
     }
 
-    public Task UploadSnapshotAsync(Snapshot snapshot, DirectoryInfo directory, ObservableTask task = null) {
+    public Task<Snapshot> CreateLocalSnapshot(DirectoryInfo localDirectory, int applicationId, string applicationPath, int versionId, string versionPath, string description, List<SnapshotTask> tasks, ITaskProgress progress) {
       return Task.Run(() => {
-        this.UploadSnapshot(snapshot, directory, task);
-      });
-    }
-
-    public Snapshot CreateLocalSnapshot(DirectoryInfo localDirectory, int applicationId, string applicationPath, int versionId, string versionPath, string description, List<SnapshotTask> tasks, ObservableTask task = null) {
-      Snapshot snapshot = null;
-      this.Reporter.StartSession(Messages.StartCreateLocalSnapshot);
-      // report initial progress
-      task?.ReportIndeterminate(this.GetMessageText(Messages.StartCreateLocalSnapshot));
-      if (localDirectory == null || !localDirectory.Exists) {
-        this.Reporter.Report(Messages.LocalDirectoryInvalid);
+        Snapshot snapshot = null;
+        this.Reporter.StartSession(Messages.StartCreateLocalSnapshot);
+        // report initial progress
+        progress?.ReportIndeterminate(this.GetMessageText(Messages.StartCreateLocalSnapshot));
+        if (localDirectory == null || !localDirectory.Exists) {
+          this.Reporter.Report(Messages.LocalDirectoryInvalid);
+          return snapshot;
+        }
+        snapshot = new Snapshot() {
+          ApplicationId = applicationId,
+          ApplicationPath = applicationPath,
+          VersionId = versionId,
+          VersionPath = versionPath,
+          Description = description,
+          Tasks = tasks
+        };
+        try {
+          var snapshotFileName = Path.Combine(localDirectory.FullName, Snapshot.FileName);
+          // remove existing snapshot file
+          if (File.Exists(snapshotFileName)) {
+            File.Delete(snapshotFileName);
+          }
+          // add files
+          foreach (var path in Directory.GetFiles(localDirectory.FullName, "*.*", SearchOption.AllDirectories)) {
+            var file = new FileInfo(path);
+            string md5;
+            using (var stream = File.OpenRead(file.FullName)) {
+              md5 = this.FtpClient.CalculateCRC(stream);
+            }
+            // create file entry
+            var entry = new SnapshotFile() {
+              RemotePath = file.FullName.Replace(localDirectory.FullName + Path.DirectorySeparatorChar, "").Replace(Path.DirectorySeparatorChar, '/'),
+              MD5 = md5,
+              Size = file.Length
+            };
+            // add entry to snapshot
+            snapshot.Files.Add(entry);
+          }
+          // add default task
+          foreach (var file in snapshot.Files) {
+            if (file.RemotePath.Contains(".exe")) {
+              snapshot.Tasks.Add(new SnapshotTask() {
+                Id = SnapshotTask.TaskId.LaunchApplication,
+                Execution = SnapshotTask.TaskExecution.Allways,
+                Files = new List<string>(new string[] {
+                                  file.RemotePath
+                              })
+              });
+              break;
+            }
+          }
+          using (var fileStream = File.Create(snapshotFileName)) {
+            var serializer = new XmlSerializer(typeof(Snapshot));
+            serializer.Serialize(fileStream, snapshot);
+          }
+        } catch {
+          this.Reporter.Report(Messages.SnapshotSerializationFailed);
+        }
         return snapshot;
-      }
-      snapshot = new Snapshot() {
-        ApplicationId = applicationId,
-        ApplicationPath = applicationPath,
-        VersionId = versionId,
-        VersionPath = versionPath,
-        Description = description,
-        Tasks = tasks
-      };
-      try {
-        var snapshotFileName = Path.Combine(localDirectory.FullName, Snapshot.FileName);
-        // remove existing snapshot file
-        if (File.Exists(snapshotFileName)) {
-          File.Delete(snapshotFileName);
-        }
-        // add files
-        foreach (var path in Directory.GetFiles(localDirectory.FullName, "*.*", SearchOption.AllDirectories)) {
-          var file = new FileInfo(path);
-          string md5;
-          using (var stream = File.OpenRead(file.FullName)) {
-            md5 = this.FtpClient.CalculateCRC(stream);
-          }
-          // create file entry
-          var entry = new SnapshotFile() {
-            RemotePath = file.FullName.Replace(localDirectory.FullName + Path.DirectorySeparatorChar, "").Replace(Path.DirectorySeparatorChar, '/'),
-            MD5 = md5,
-            Size = file.Length
-          };
-          // add entry to snapshot
-          snapshot.Files.Add(entry);
-        }
-        // add default task
-        foreach (var file in snapshot.Files) {
-          if (file.RemotePath.Contains(".exe")) {
-            snapshot.Tasks.Add(new SnapshotTask() {
-              Id = SnapshotTask.TaskId.LaunchApplication,
-              Execution = SnapshotTask.TaskExecution.Allways,
-              Files = new List<string>(new string[] {
-                                file.RemotePath
-                            })
-            });
-            break;
-          }
-        }
-        using (var fileStream = File.Create(snapshotFileName)) {
-          var serializer = new XmlSerializer(typeof(Snapshot));
-          serializer.Serialize(fileStream, snapshot);
-        }
-      } catch {
-        this.Reporter.Report(Messages.SnapshotSerializationFailed);
-      }
-      return snapshot;
-    }
-
-    public Task<Snapshot> CreateLocalSnapshotAsync(DirectoryInfo localDirectory, int applicationId, string applicationPath, int versionId, string versionPath, string description, List<SnapshotTask> tasks, ObservableTask task = null) {
-      return Task.Run(() => {
-        return this.CreateLocalSnapshot(localDirectory, applicationId, applicationPath, versionId, versionPath, description, tasks, task);
       });
     }
 
-    public void DownloadSnapshot(Snapshot snapshot, DirectoryInfo directory, ObservableTask task = null) {
+    public async Task DownloadSnapshot(Snapshot snapshot, DirectoryInfo directory, CancellationToken cancellationToken, ITaskProgress progress) {
       this.Reporter.StartSession(Messages.StartDownloadSnapshot);
       // report initial progress
-      task?.ReportIndeterminate(this.GetMessageText(Messages.StartDownloadSnapshot));
+      progress?.ReportIndeterminate(this.GetMessageText(Messages.StartDownloadSnapshot));
       // check snapshot
       if (snapshot == null) {
         this.Reporter.Report(Messages.InvalidSnapshot);
@@ -309,16 +293,16 @@ namespace HAF.Updates {
       if (this.Reporter.HasErrors) {
         return;
       }
-      task?.ReportProgress(0, snapshot.Files.Count + 2);
+      progress?.ReportProgress(0, snapshot.Files.Count + 2);
       foreach (var file in snapshot.Files) {
         var tries = 0;
         while (true) {
           if (this.Reporter.HasErrors) {
             return;
           }
-          var success = this.FtpClient.DownloadToFileVerified(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, file.RemotePath), Path.Combine(directory.FullName, file.LocalPath), file.MD5, file.Size);
+          var success = await this.FtpClient.DownloadToFileVerified(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, file.RemotePath), Path.Combine(directory.FullName, file.LocalPath), file.MD5, cancellationToken, file.Size, progress);
           if (success) {
-            task?.ReportProgress();
+            progress?.ReportProgress();
             break;
           } else {
             tries++;
@@ -330,38 +314,29 @@ namespace HAF.Updates {
         }
       }
       // download snapshot
-      this.FtpClient.DownloadToFile(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, Snapshot.FileName), Path.Combine(directory.FullName, Snapshot.FileName));
-      task?.ReportProgress();
+      await this.FtpClient.DownloadToFile(FtpClient.CombinePath(snapshot.ApplicationPath, snapshot.VersionPath, Snapshot.FileName), Path.Combine(directory.FullName, Snapshot.FileName), cancellationToken, progress: progress);
+      progress?.ReportProgress();
       // download update controller
       if (File.Exists(SnapshotClient.UpdateControllerName)) {
         File.Delete(SnapshotClient.UpdateControllerName);
       }
-      this.FtpClient.DownloadToFile(SnapshotClient.UpdateControllerName, SnapshotClient.UpdateControllerName);
-      task?.ReportProgress();
+      await this.FtpClient.DownloadToFile(SnapshotClient.UpdateControllerName, SnapshotClient.UpdateControllerName, cancellationToken, progress: progress);
+      progress?.ReportProgress();
     }
 
-    public Task DownloadSnapshotAsync(Snapshot snapshot, DirectoryInfo directory, ObservableTask task = null) {
+    public Task<Snapshot> ReadLocalSnapshot(string localDirectory, ITaskProgress progress) {
       return Task.Run(() => {
-        this.DownloadSnapshot(snapshot, directory, task);
-      });
-    }
-
-    public Snapshot ReadLocalSnapshot(string localDirectory) {
-      this.Reporter.StartSession(Messages.StartReadLocalSnapshot);
-      try {
-        using (var fileStream = File.OpenRead(Path.Combine(localDirectory, Snapshot.FileName))) {
-          var serializer = new XmlSerializer(typeof(Snapshot));
-          return (Snapshot)serializer.Deserialize(fileStream);
+        progress?.ReportIndeterminate();
+        this.Reporter.StartSession(Messages.StartReadLocalSnapshot);
+        try {
+          using (var fileStream = File.OpenRead(Path.Combine(localDirectory, Snapshot.FileName))) {
+            var serializer = new XmlSerializer(typeof(Snapshot));
+            return (Snapshot)serializer.Deserialize(fileStream);
+          }
+        } catch {
+          this.Reporter.Report(Messages.SnapshotSerializationFailed);
+          return null;
         }
-      } catch {
-        this.Reporter.Report(Messages.SnapshotSerializationFailed);
-        return null;
-      }
-    }
-
-    public Task<Snapshot> ReadLocalSnapshotAsync(string localDirectory) {
-      return Task.Run(() => {
-        return this.ReadLocalSnapshot(localDirectory);
       });
     }
 
