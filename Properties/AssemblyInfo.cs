d5425024d9bcaa88766d3632f9afea4646851cdd
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-09-04 23:43:44 +0200

    added default prefix

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Windows.Markup;
 [assembly: Guid("2350a0ff-7af9-4a41-aa57-00bf4ea22c84")]
 [assembly: AssemblyVersion("1.0.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
+[assembly: XmlnsPrefix("http://www.hornung.dev/xaml/presentation", "haf")]
 [assembly: XmlnsDefinition("http://www.hornung.dev/xaml/presentation", "HAF")]
 [assembly: XmlnsDefinition("http://www.hornung.dev/xaml/presentation", "HAF.Controls")]
 [assembly: XmlnsDefinition("http://www.hornung.dev/xaml/presentation", "HAF.Converters")]
