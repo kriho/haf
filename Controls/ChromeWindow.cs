204a0bc2ae7c66417205fe9190b5441ef930612b
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-09-10 08:05:26 +0200

    added new property to ChromeWindow

--- Controls/ChromeWindow.cs
+++ Controls/ChromeWindow.cs
@@ -5,16 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace HAF.Controls {
   public class ChromeWindow: Window {
+    
+    public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(object), typeof(ChromeWindow), new UIPropertyMetadata(null));
 
     public object Header {
       get { return (object)GetValue(HeaderProperty); }
       set { SetValue(HeaderProperty, value); }
     }
 
-    public static readonly DependencyProperty HeaderProperty = DependencyProperty.Register("Header", typeof(object), typeof(ChromeWindow), new UIPropertyMetadata(null));
+    public static readonly DependencyProperty TitleIconProperty = DependencyProperty.Register("TitleIcon", typeof(ImageSource), typeof(ChromeWindow), new UIPropertyMetadata(null));
+
+    public ImageSource TitleIcon {
+      get { return (ImageSource)GetValue(TitleIconProperty); }
+      set { SetValue(TitleIconProperty, value); }
+    }
 
     static ChromeWindow() {
       DefaultStyleKeyProperty.OverrideMetadata(typeof(ChromeWindow), new FrameworkPropertyMetadata(typeof(ChromeWindow)));
