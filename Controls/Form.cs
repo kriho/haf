6b9da15a50a18fcedcbb5c4708234e727896d170
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-09-23 08:16:51 +0200

    updated form controls

--- Controls/Form.cs
+++ Controls/Form.cs
@@ -7,10 +7,24 @@ using System.Windows;
 using System.Windows.Controls;
 
 namespace HAF.Controls {
-  public class Form: ContentControl {
+  public class Form: ItemsControl {
 
     static Form() {
       DefaultStyleKeyProperty.OverrideMetadata(typeof(Form), new FrameworkPropertyMetadata(typeof(Form)));
     }
+
+    public ScrollBarVisibility HorizontalScrollBarVisibility {
+      get { return (ScrollBarVisibility)GetValue(HorizontalScrollBarVisibilityProperty); }
+      set { SetValue(HorizontalScrollBarVisibilityProperty, value); }
+    }
+
+    public static readonly DependencyProperty HorizontalScrollBarVisibilityProperty = DependencyProperty.Register("HorizontalScrollBarVisibility", typeof(ScrollBarVisibility), typeof(Form), new PropertyMetadata(ScrollBarVisibility.Disabled));
+
+    public ScrollBarVisibility VerticalScrollBarVisibility {
+      get { return (ScrollBarVisibility)GetValue(VerticalScrollBarVisibilityProperty); }
+      set { SetValue(VerticalScrollBarVisibilityProperty, value); }
+    }
+
+    public static readonly DependencyProperty VerticalScrollBarVisibilityProperty = DependencyProperty.Register("VerticalScrollBarVisibility", typeof(ScrollBarVisibility), typeof(Form), new PropertyMetadata(ScrollBarVisibility.Visible));
   }
 }
