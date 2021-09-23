4e9e0cf34a468609ecbb3bea609e6dcd98d6fd14
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-09-23 08:18:11 +0200

    added more converters

---
+++ Converters/BooleanToSolidColorBrushConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace HAF.Converters {
+  public class BooleanToSolidColorBrushConverter: ValueConverter<bool, SolidColorBrush> {
+    public Color TrueValue { get; set; }
+    public Color FalseValue { get; set; }
+
+    protected override SolidColorBrush convert(bool value) {
+      return new SolidColorBrush(value ? this.TrueValue : this.FalseValue);
+    }
+  }
+}
---
+++ Converters/DoubleToFormattedUnitConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Media;
+
+namespace HAF.Converters {
+  public class DoubleToFormattedUnitConverter: ValueConverter<double, string> {
+    public string Unit { get; set; } = "m";
+    public double Factor { get; set; } = 1;
+    public string Format { get; set; } = "{0:0.00}";
+
+    protected override string convert(double value) {
+      return string.Format(this.Format, value * this.Factor) + (this.Unit != "" ? " " + this.Unit : "");
+    }
+  }
+}
