a85035b151d0f04f07b06863e01316c53b522705
Kristjan Hornung <kristjan.hornung@de.abb.com>
2021-09-24 23:50:50 +0200

    updated validation support

--- Observable/ValidatableObservableObject.cs
+++ Observable/ValidatableObservableObject.cs
@@ -15,52 +15,79 @@ using System.Windows;
 namespace HAF {
   public abstract class ValidatableObservableObject: ObservableObject, INotifyDataErrorInfo {
 
-    protected bool SetValueAndValidate<T>(ref T property, T value, Func<IEnumerable<string>> validation, bool subscribe = false, [CallerMemberName] string propertyName = "") {
-      if (!this.SetValue(ref property, value, subscribe, propertyName)) {
-        return false;
-      }
-      // perform validation
+    private ValidationBatch validationBatch = new ValidationBatch();
+    private readonly object validationLock = new object();
+    private readonly ConcurrentDictionary<string, List<string>> validationErrors = new ConcurrentDictionary<string, List<string>>();
+
+    private void ValidateBatch(string propertyName, Action<ValidationBatch> batch) {
       lock(this.validationLock) {
-        var validationErrors = validation.Invoke().ToList();
-        if(this.errors.TryGetValue(propertyName, out var list)) {
-          if(validationErrors.Count == 0) {
-            if(list.Count > 0) {
-              list.Clear();
-              this.OnErrorsChanged(propertyName);
+        // prepare batch
+        this.validationBatch.PropertyName = propertyName;
+        this.validationBatch.Errors.Clear();
+        // execute assertions
+        batch.Invoke(this.validationBatch);
+        // apply batch
+        if(this.validationErrors.TryGetValue(this.validationBatch.PropertyName, out var oldErrors)) {
+          if(this.validationBatch.Errors.Count == 0) {
+            if(oldErrors.Count > 0) {
+              oldErrors.Clear();
+              this.OnErrorsChanged(this.validationBatch.PropertyName);
               this.NotifyPropertyChanged(() => this.HasErrors);
             }
           } else {
-            list.Clear();
-            list.AddRange(validationErrors);
-            this.OnErrorsChanged(propertyName);
+            oldErrors.Clear();
+            oldErrors.AddRange(this.validationBatch.Errors);
+            this.OnErrorsChanged(this.validationBatch.PropertyName);
             this.NotifyPropertyChanged(() => this.HasErrors);
           }
-        } else if(validationErrors.Count > 0) {
-          _ = this.errors.TryAdd(propertyName, validationErrors);
-          this.OnErrorsChanged(propertyName);
+        } else if(this.validationBatch.Errors.Count > 0) {
+          _ = this.validationErrors.TryAdd(this.validationBatch.PropertyName, new List<string>(this.validationBatch.Errors));
+          this.OnErrorsChanged(this.validationBatch.PropertyName);
           this.NotifyPropertyChanged(() => this.HasErrors);
         }
       }
+    }
+
+    protected bool SetValueAndValidate<T>(ref T property, T value, Action<ValidationBatch> batch, bool subscribe = false, [CallerMemberName] string propertyName = "") {
+      if (!this.SetValue(ref property, value, subscribe, propertyName)) {
+        return false;
+      }
+      this.ValidateBatch(propertyName, batch);
       return true;
     }
 
-    protected bool SetValueAndValidate<T>(ref T property, T value, Func<IEnumerable<string>> validation, Expression<Func<T>> expression, bool subscribe = false) {
-      return this.SetValueAndValidate(ref property, value, validation, subscribe, this.GetExpressionMemberName(expression));
+    protected bool SetValueAndValidate<T>(ref T property, T value, Action<ValidationBatch> batch, Expression<Func<T>> expression, bool subscribe = false) {
+      return this.SetValueAndValidate(ref property, value, batch, subscribe, this.GetExpressionMemberName(expression));
     }
 
-    private readonly ConcurrentDictionary<string, List<string>> errors = new ConcurrentDictionary<string, List<string>>();
-    private readonly object validationLock = new object();
+    protected bool SetValueAndValidateObject<T>(ref T property, T value, bool subscribe = false, [CallerMemberName] string propertyName = "") {
+      if(!this.SetValue(ref property, value, subscribe, propertyName)) {
+        return false;
+      }
+      this.ValidateBatch("", this.ValidateObject);
+      return true;
+    }
+
+    protected bool SetValueAndValidateObject<T>(ref T property, T value, Expression<Func<T>> expression, bool subscribe = false) {
+      return this.SetValueAndValidateObject(ref property, value, subscribe, this.GetExpressionMemberName(expression));
+    }
+
+    protected virtual void ValidateObject(ValidationBatch batch) {
+    }
 
     public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
 
     public bool HasErrors {
       get {
-        return this.errors.Any(e => e.Value != null && e.Value.Count > 0);
+        return this.validationErrors.Any(e => e.Value != null && e.Value.Count > 0);
       }
     }
 
     public IEnumerable GetErrors(string propertyName) {
-      return this.errors.TryGetValue(propertyName, out var errorsForProperty) ? errorsForProperty : Enumerable.Empty<string>();
+      // https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.inotifydataerrorinfo?view=net-5.0
+      // empty propertyName or null indicates that entity level errors shell be retrieved
+      this.validationErrors.TryGetValue(propertyName  ?? "", out var errorsForProperty);
+      return errorsForProperty ?? Enumerable.Empty<string>();
     }
 
     public void OnErrorsChanged(string propertyName) {
---
+++ Observable/ValidationBatch.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAF {
+
+  public class ValidationBatch {
+    internal List<string> Errors = new List<string>();
+    internal string PropertyName;
+
+    public void Assert(bool assertion, string error) {
+      if(!assertion) {
+        this.Errors.Add(error);
+      }
+    }
+  }
+}
