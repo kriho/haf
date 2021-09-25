16c561f3db16e92ef8f0344ab2bd05d4f4ed1f94
Kristjan Hornung <kristjan.hornung@outlook.com>
2021-09-25 22:56:35 +0200

    introduces easy access to log service

---
+++ Logging/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAF {
+  public static class Log {
+    internal static readonly ILogService LogService;
+
+    static Log() {
+      Log.LogService = Configuration.Container.GetExportedValue<ILogService>();
+    }
+
+    public static void Info(string message) {
+      Log.LogService.Info(message);
+    }
+
+    public static void Warning(string message) {
+      Log.LogService.Warning(message);
+    }
+
+    public static void Error(string message) {
+      Log.LogService.Error(message);
+    }
+  }
+}
