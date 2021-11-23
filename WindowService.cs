0be8be4e48f1581f5527d23923602c78dd5e6116
Kristjan Hornung <kristjan.hornung@de.abb.com>
2021-11-23 20:14:11 +0100

    added control cache

--- WindowService.cs
+++ WindowService.cs
@@ -12,10 +12,36 @@ namespace HAF {
 
   [Export(typeof(IWindowService)), PartCreationPolicy(CreationPolicy.Shared)]
   public class WindowService : Service, IWindowService {
+    private Dictionary<string, object> controls = new Dictionary<string,object>();
 
     public Window Window { get; set; }
 
-    public override void LoadConfiguration(ServiceConfiguration configuration) {
+    public bool TryGetControl<T>(string name, out T control) {
+      if(this.controls.TryGetValue(name, out var entry) && entry is T typedEntry) {
+        control = typedEntry;
+        return true;
+      }
+      control = default(T);
+      return false;
+    }
+
+
+    public T GetControl<T>(string name) {
+      if(this.TryGetControl<T>(name, out var control)) {
+        return control;
+      } else {
+        throw new Exception($"the control \"{name}\" was accessed before its registration in the window service");
+      }
+    }
+
+    public void RegisterControl<T>(string name, T control) {
+      if(this.controls.ContainsKey(name)) {
+        throw new InvalidOperationException($"the control with name \"{name}\" was already registered");
+      }
+      this.controls.Add(name, control);
+    }
+
+    public override Task LoadConfiguration(ServiceConfiguration configuration) {
       if (configuration.TryReadEntry("window", out var window)) {
         this.Window.Topmost = window.ReadAttribute("topmost", false);
         if (window.TryReadAttribute("width", out int width)) {
@@ -25,13 +51,15 @@ namespace HAF {
           this.Window.Height = height;
         }
       }
+      return Task.CompletedTask;
     }
 
-    public override void SaveConfiguration(ServiceConfiguration configuration) {
+    public override Task SaveConfiguration(ServiceConfiguration configuration) {
       configuration.WriteEntry("window", true)
         .WriteAttribute("topmost", this.Window.Topmost)
         .WriteAttribute("width", (int)this.Window.Width)
-        .WriteAttribute("height", (int)this.Window.Height);
+        .WriteAttribute("height", (int)this.Window.Height); 
+      return Task.CompletedTask;
     }
   }
 }
