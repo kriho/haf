88d05a5fe49a413504b0daa8ddf0f96d33126864
Kristjan Hornung <kristjan.hornung@de.abb.com>
2021-11-23 20:10:19 +0100

    added helper for exceptions

---
+++ Utils/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HAF {
+  public static partial class Utils {
+    public static string GetExceptionDescription(Exception e) {
+      var description = new StringBuilder();
+      description.Append(e.Message);
+      while(e.InnerException != null) {
+        description.Append(" / " + e.InnerException.Message);
+        e = e.InnerException;
+      }
+      return description.ToString();
+    }
+  }
+}
