140133b08702cb290507e53da6d410e26edfbde4
Kristjan Hornung <kristjan.hornung@de.abb.com>
2021-11-26 11:40:37 +0100

    added new converter

---
+++ Converters/StringDefaultConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HAF.Converters {
+  public class StringDefaultConverter: ValueConverter<string, string> {
+    public string Default { get; set; } = string.Empty;
+
+    protected override string convert(string value) {
+     return string.IsNullOrWhiteSpace(value) ? this.Default : value;
+    }
+  }
+}
