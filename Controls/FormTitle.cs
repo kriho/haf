3e7ab6455c11212ee54a0afcb1e774018caf691a
Kristjan Hornung <kristjan.hornung@de.abb.com>
2021-11-26 11:40:59 +0100

    updated form, added form title

---
+++ Controls/FormTitle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HAF.Controls {
+  public class FormTitle: Control {
+
+    public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(FormTitle), new PropertyMetadata("Title"));
+
+    public string Title {
+      get { return (string)GetValue(TitleProperty); }
+      set { SetValue(TitleProperty, value); }
+    }
+
+    static FormTitle() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(FormTitle), new FrameworkPropertyMetadata(typeof(FormTitle)));
+
+    }
+  }
+}
