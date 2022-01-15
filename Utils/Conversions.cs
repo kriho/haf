44c6e2a7610c132ffa5b80aeb3b26764fd8735dd
Kristjan Hornung <kristjan.hornung@outlook.com>
2022-01-15 15:16:58 +0100

    added byte to hex string

--- Utils/Conversions.cs
+++ Utils/Conversions.cs
@@ -12,5 +12,18 @@ namespace HAF {
                        .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                        .ToArray();
     }
+
+
+    public static string ByteArrayToHexadecimalString(byte[] buffer, bool addPrefix = true, int offset = 0, int? length = 0) {
+      var usedLength = length.HasValue ? length.Value : buffer.Length;
+      var hex = new StringBuilder(usedLength * 2 + (addPrefix ? 2 : 0));
+      if(addPrefix) {
+        hex.Append("0x");
+      }
+      for(var index = 0; index < usedLength; index++) {
+        hex.AppendFormat("{0:X2}", buffer[offset + index]);
+      }
+      return hex.ToString();
+    }
   }
 }
