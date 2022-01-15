a23776f2c691d32e688c0a11aaddf37d1dd3cbe5
Kristjan Hornung <kristjan.hornung@outlook.com>
2022-01-15 15:19:08 +0100

    added substring converter

---
+++ Converters/StringToSubstringConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAF.Converters {
+  public class StringToSubstringConverter: ValueConverter<string, string> {
+    public int Index { get; set; }
+
+    public int Length { get; set; }
+
+    protected override string convert(string value) {
+      if(value == null) {
+        return null;
+      }
+      return value.Substring(this.Index, this.Length);
+    }
+  }
+}
