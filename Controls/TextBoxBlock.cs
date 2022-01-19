bcd5d80689c64d4abbe6aa34e1001cabbbf77413
Kristjan Hornung <kristjan.hornung@outlook.com>
2022-01-19 23:02:30 +0100

    added TextBoxBlock, which looks like a TextBlock but supports text selection and copying

---
+++ Controls/TextBoxBlock.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HAF.Controls {
+  public class TextBoxBlock: TextBox {
+
+    static TextBoxBlock() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(TextBoxBlock), new FrameworkPropertyMetadata(typeof(TextBoxBlock)));
+    }
+  }
+}
