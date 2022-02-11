8bd4ef1352977284bcf93b66a244e1c76154b322
Kristjan Hornung <kristjan.hornung@outlook.com>
2022-02-11 20:07:14 +0100

    added bindable TextBlock control

---
+++ Controls/FastBindableTextBlock.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace HAF.Controls {
+  public class FastBindableTextBlock: TextBlock {
+    public IEnumerable<Inline> BindableInlines {
+      get { return (IEnumerable<Inline>)GetValue(BindableInlinesProperty); }
+      set { SetValue(BindableInlinesProperty, value); }
+    }
+
+    public static readonly DependencyProperty BindableInlinesProperty = DependencyProperty.Register("BindableInlines", typeof(IEnumerable<Inline>), typeof(FastBindableTextBlock), new UIPropertyMetadata(null, OnBindableInlinesChanged));
+
+    private static void OnBindableInlinesChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) {
+      if(sender is FastBindableTextBlock bindableTextBlock) {
+        if(e.NewValue is IEnumerable<Inline> inlines) {
+          bindableTextBlock.Inlines.Clear();
+          bindableTextBlock.Inlines.AddRange(inlines);
+        }
+      }
+    }
+  }
+}
