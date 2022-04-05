afb236ed14a67fec79987dba75b01acd4ae70ffd
Kristjan Hornung <kristjan.hornung@outlook.com>
2022-04-05 23:56:51 +0200

    added ScrollingForm

---
+++ Controls/ScrollingForm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace HAF.Controls {
+  public class ScrollingForm: Form {
+    static ScrollingForm() {
+      DefaultStyleKeyProperty.OverrideMetadata(typeof(ScrollingForm), new FrameworkPropertyMetadata(typeof(ScrollingForm)));
+    }
+  }
+}
