63a4da185eab52fd05fc349aaf4f9e5fdc3a9e11
Kristjan Hornung <kristjan.hornung@outlook.com>
2022-04-05 23:58:16 +0200

    added converters

---
+++ Converters/IntegerToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAF.Converters {
+  public class IntegerToStringConverter: ValueConverter<int, string> {
+    public int Base { get; set; } = 10;
+
+    protected override string convert(int value) {
+      return System.Convert.ToString(value, this.Base);
+    }
+
+    protected override int convertBack(string value) {
+      return System.Convert.ToInt32(value, this.Base); 
+    }
+  }
+}
---
+++ Converters/StringToIntegerConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HAF.Converters {
+  public class StringToIntegerConverter: ValueConverter<string, int> {
+    public int Base { get; set; } = 10;
+
+    protected override int convert(string value) {
+      return System.Convert.ToInt32(value, this.Base);
+    }
+
+    protected override string convertBack(int value) {
+      return System.Convert.ToString(value, this.Base);
+    }
+  }
+}
