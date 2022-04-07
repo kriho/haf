8395b712dfafec8a6f5e5a3f09a7e4a23a0fa77d
Kristjan Hornung <kristjan.hornung@outlook.com>
2022-04-07 21:20:12 +0200

    efficient IReadOnlyState for representing a fixed state

---
+++ States/FixedState.cs
@@ -0,0 +1,41 @@
+﻿using HAF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace HAF {
+  /// <summary>
+  /// Represents a fixed state.
+  /// </summary>
+  public class FixedState: IReadOnlyState {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public bool Value { get; private set; }
+
+    private FixedState negated;
+    public IReadOnlyState Negated {
+      get {
+        if(this.negated == null) {
+          this.negated = new FixedState(!this.Value);
+        }
+        return this.negated;
+      }
+    }
+
+    public void RegisterUpdate(Action callback) {
+    }
+
+    public FixedState(bool value) {
+      this.Value = value;
+    }
+
+    public static implicit operator bool(FixedState state) {
+      return state.Value;
+    }
+  }
+}
