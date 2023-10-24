6dbd178aa32f8a6f0abf4fd87b1c58c849628f16
Kristjan Hornung <kristjan.hornung@outlook.com>
2023-10-24 09:16:44 +0200

    added diagnostic severity

--- Logging/LogSeverity.cs
+++ Logging/LogSeverity.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace HAF {
   public class LogSeverity: Enumeration {
 
+    public static LogSeverity Diagnostic = new LogSeverity(0, "Diagnostic");
     public static LogSeverity Info = new LogSeverity(1, "Info");
     public static LogSeverity Warning = new LogSeverity(2, "Warning");
     public static LogSeverity Error = new LogSeverity(3, "Error");
@@ -17,6 +18,7 @@ namespace HAF {
 
     public static LogSeverity Parse(string value) {
       switch (value.ToLower()) {
+        case "diagnostic": return LogSeverity.Diagnostic;
         case "info": return LogSeverity.Info;
         case "warning": return LogSeverity.Warning;
         default: return LogSeverity.Error;
